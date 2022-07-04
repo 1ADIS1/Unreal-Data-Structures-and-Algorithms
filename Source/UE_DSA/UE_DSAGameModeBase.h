2f4f6067aff608c49ca4fc93cba32ce7dc864243
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2022-07-04 21:07:03 +0300

    Добавьте файлы проекта.

---
+++ Source/UE_DSA/UE_DSAGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_DSAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_DSA_API AUE_DSAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
